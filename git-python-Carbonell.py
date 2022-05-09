56e300815021973c035f15b29adf50c27e0db427
Roi Jewel H. Carbonell <Crbnll12200@gmail.com>
2022-05-09 17:54:31 +0800

    My Educational Background added

--- git-python-Carbonell.py
+++ git-python-Carbonell.py
@@ -1,6 +1,13 @@
 MyName = "Roi Jewel H. Carbonell"
 MyAge = 22
 MyAddress = "Cawit, Boac, Marinduque"
+MyCollege = "Marinduque State College"
+MyCourse = "BSIT"
+MySection = "2-C"
+
 print("Name: " +MyName.upper())
 print("Address: " + str(MyAge))
 print("Birth day: " +MyAddress.upper())
+print("University: " +MyCollege)
+print("Course: " +MyCourse)
+print("Section: " +MySection)
