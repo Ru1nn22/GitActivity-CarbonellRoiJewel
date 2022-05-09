1a6a5ecf7ea9e744c6d0ccdb6cd260fd968f158f
Roi Jewel H. Carbonell <Crbnll12200@gmail.com>
2022-05-09 18:08:15 +0800

    My Hobbies Added

--- git-python-Carbonell.py
+++ git-python-Carbonell.py
@@ -5,9 +5,17 @@ MyCollege = "Marinduque State College"
 MyCourse = "BSIT"
 MySection = "2-C"
 
+print("MY PERSONAL INFORMATION")
 print("Name: " +MyName.upper())
 print("Address: " + str(MyAge))
 print("Birth day: " +MyAddress.upper())
+print("MY EDUCATIONAL BACKGROUND")
 print("University: " +MyCollege)
 print("Course: " +MyCourse)
 print("Section: " +MySection)
+print("MY HOBBIES")
+print("Reading Manga")
+print("Watching Anime")
+print("Playing Red Dead Redemption 2")
+print("Playing The Sims 4")
+print("Watching Anything about financial Literacy on Youtube")
