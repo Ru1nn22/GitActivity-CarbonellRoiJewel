69454f6a853fd33c01913e141e64ea11e936f891
Roi Jewel H. Carbonell <Crbnll12200@gmail.com>
2022-05-09 17:41:37 +0800

    My Personal Info added

---
+++ git-python-Carbonell.py
@@ -0,0 +1,6 @@
+MyName = "Roi Jewel H. Carbonell"
+MyAge = 22
+MyAddress = "Cawit, Boac, Marinduque"
+print("Name: " +MyName.upper())
+print("Address: " + str(MyAge))
+print("Birth day: " +MyAddress.upper())
